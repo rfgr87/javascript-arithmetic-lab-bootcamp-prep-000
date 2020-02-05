31860c0c75252f86a536b37ba1cd1fd1473476f6
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-05 17:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-return 1+80
+var number = 10
+
+function add5() {
+  number += add5
+}
+
+function divideBy3(){
+  number /= 3
+}
+
+divideBy3()
+
+console.log(number)
+
+add5()
+
+console.log(number)
+
+number = 10
+
+add5()
+
+console.log(number)
+
+divideBy3()
+
+console.log(number)
